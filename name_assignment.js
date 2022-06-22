76f50ab9b6c40ac7e800dfbb37c444dbe90c3c89
godwin <okpegodwin18@yahoo.com>
2022-06-22 19:03:57 +0100

    Ssubmitting my assignment for JS name, height and country display output

---
+++ name_assignment.js
@@ -0,0 +1,25 @@
+let my_Name = "Godwin Okpe";
+let height = "172 cm";
+let country = "Nigeria";
+
+alert(
+  " My name is " +
+    my_Name +
+    ". I am " +
+    height +
+    " tall, " +
+    " and I come from " +
+    country +
+    "."
+);
+
+document.write(
+  " My name is " +
+    my_Name +
+    ". I am " +
+    height +
+    " tall, " +
+    " and I come from " +
+    country +
+    "."
+);
